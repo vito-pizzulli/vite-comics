665574d7c00606694317dd52b16701ed049959d0
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-15 16:33:18 +0200

    added header links objects array with dynamic population

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,24 +6,66 @@
         </div>
         <nav id="header-navbar">
             <ul>
-                <li>Prova</li>
-                <li>Prova</li>
-                <li>Prova</li>
-                <li>Prova</li>
-                <li>Prova</li>
-                <li>Prova</li>
-                <li>Prova</li>
-                <li>Prova</li>
+                <li v-for="link in navbarLinks"><a :href="link.link">{{ link.text }}</a></li>
             </ul>
         </nav>
         </div>
     </header>
 </template>
+
 <script>
 export default {
-    name: 'AppHeader'
+    name: 'AppHeader',
+
+    data() {
+            return {
+                navbarLinks: [
+                    {
+                        text: 'characters',
+                        link: '#'
+                    },
+                    {
+                        text: 'comics',
+                        link: '#'
+                    },
+                    {
+                        text: 'movies',
+                        link: '#'
+                    },
+                    {
+                        text: 'tv',
+                        link: '#'
+                    },
+                    {
+                        text: 'games',
+                        link: '#'
+                    },
+                    {
+                        text: 'collectibles',
+                        link: '#'
+                    },
+                    {
+                        text: 'videos',
+                        link: '#'
+                    },
+                    {
+                        text: 'fans',
+                        link: '#'
+                    },
+                    {
+                        text: 'news',
+                        link: '#'
+                    },
+                    {
+                        text: 'shop',
+                        link: '#'
+                    },
+                ]
+            }
+        }
 }
 </script>
+
 <style lang="scss" scoped>
     @use '../styles/general' as *;
     @use '../styles/partials/variables' as *;
@@ -31,7 +73,6 @@ export default {
 
     header {
         height: 150px;
-        background-color: red;
         
         div.wrapper {
             @include flex(row, space-around, center, no-wrap);
