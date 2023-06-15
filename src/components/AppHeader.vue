d8f6dbdb98f7f78375537d80067197ecfa358e2e
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-15 15:16:54 +0200

    AppHeader structure completed

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,20 @@
 <template>
     <header>
-        <h1>AppHeader</h1>
+        <div id="header-logo">
+            <img src="../assets/img/dc-logo.png" alt="DC Comics Logo">
+        </div>
+        <nav id="header-navbar">
+            <ul>
+                <li>Prova</li>
+                <li>Prova</li>
+                <li>Prova</li>
+                <li>Prova</li>
+                <li>Prova</li>
+                <li>Prova</li>
+                <li>Prova</li>
+                <li>Prova</li>
+            </ul>
+        </nav>
     </header>
 </template>
 <script>
@@ -10,4 +24,24 @@ export default {
 </script>
 <style lang="scss" scoped>
     @use '../styles/partials/variables' as *;
+    @use '../styles/partials/mixins' as *;
+
+    header {
+        @include flex(row, space-around, center, no-wrap);
+        @include w-75-centered;
+        height: 150px;
+        background-color: red;
+        
+        nav#header-navbar {
+
+            ul {
+            @include flex(row, space-between, center, no-wrap);
+            list-style: none;
+
+                li {
+                    margin: 1rem;
+                }
+            }
+        }
+    }
 </style>
