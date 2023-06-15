ede838463e1eae420f5a610cc50be105b976256b
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-15 14:42:28 +0200

    imported components in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
   <div>
-    
+    <AppHeader />
+    <AppMain />
+    <AppFooter />
   </div>
 </template>
 <script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
 export default {
-  
+  name: 'App',
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
 }
 </script>
 <style lang="scss">
-  
+
 </style>
