39cc0dad80543c9b3c0241816fd60cd8802f16bf
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-16 14:44:49 +0200

    created AppComicsElement.vue, imported styles scss files

---
+++ src/components/AppComicsElement.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="comics-element">
+        <div class="wrapper">
+            <h1>Prova</h1>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'AppComicsElement'
+
+}
+</script>
+<style lang="scss">
+    @use '../styles/general' as *;
+    @use '../styles/partials/variables' as *;
+    @use '../styles/partials/mixins' as *;
+</style>
