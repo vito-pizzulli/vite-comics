fb2c655c5f494c99c7093c9a3f733970471be811
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-16 18:04:21 +0200

    created AppJumbotron.vue containing the jumbotron with styling

---
+++ src/components/AppJumbotron.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="jumbotron">
+        <img src="../assets/img/jumbotron.jpg" alt="DC Comics Jumbotron">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppJumbotron'
+}
+</script>
+
+<style lang="scss">
+    @use '../styles/general' as *;
+    @use '../styles/partials/variables' as *;
+    @use '../styles/partials/mixins' as *;
+
+    div#jumbotron {
+        height: 600px;
+
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            object-position: top;
+        }
+    }
+</style>
