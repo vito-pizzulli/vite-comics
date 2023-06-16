afc3ab0acbecfb5c087b521c064cb6adb0bb0b7a
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-16 14:37:09 +0200

    created AppComicsList.vue, imported all scss files

---
+++ src/components/AppComicsList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="comics-list">
+        <h1>Prova</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'AppComicsList'
+}
+</script>
+<style lang="scss" scoped>
+    @use '../styles/general' as *;
+    @use '../styles/partials/variables' as *;
+    @use '../styles/partials/mixins' as *;
+</style>
